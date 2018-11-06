6505471de07935e2569fad8ae724cdd81e2aa848
Patrick A Kim <pkim050@gmail.com>
2018-11-06 17:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.splice(-1, 1);
+  return kittens.splice(kittens.length - 1, 1);
 }
 
 function removeFirstKitten() {
