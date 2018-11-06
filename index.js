7a53f3cb60532d93b67b03a8de619bc1b9151537
Patrick A Kim <pkim050@gmail.com>
2018-11-06 16:57:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  name.push("kitten");
+  return name;
+}
+
+function destrivelyPrependKitten(name) {
+  name.shift("kitten");
+  return name;
+}
+
+function destructivelyRemoveLastKitten() {
+  name.pop();
+  return name;
+}
+
+function destructivelyRemoveFirstKitten() {
+  name.unshift();
+  return name;
+}
+
+function appendKitten(name) {
+  
+}
